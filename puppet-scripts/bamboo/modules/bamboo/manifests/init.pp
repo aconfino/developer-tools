b0c1fd21e71c38b451ed370c9b7d5310ae9373aa
Cloud User <ec2-user@ip-10-0-0-23.us-west-2.compute.internal>
2015-04-12 13:32:40 -0400

    added stdlib

--- puppet-scripts/bamboo/modules/bamboo/manifests/init.pp
+++ puppet-scripts/bamboo/modules/bamboo/manifests/init.pp
@@ -1,9 +1,11 @@
+
 class bamboo ($bamboo_base_dir, $version){
 
 $bamboo_home="$bamboo_base_dir/bamboo-home"
 $bamboo_install_dir="$bamboo_base_dir/atlassian-bamboo-$version"
 $tarball="atlassian-bamboo-$version.tar.gz"
- 
+$setenv="$bamboo_install_dir/bin/setenv.sh"
+
    file { $bamboo_base_dir :
       ensure => directory,
 	  owner => "ec2-user",
@@ -41,10 +43,15 @@ $tarball="atlassian-bamboo-$version.tar.gz"
     require => Exec["extract_bamboo"],
 	notify => Service["bamboo"]
   }
- 
-  file { "/etc/environment":
-    content => inline_template("BAMBOO_HOME=$bamboo_home"),
+
+
+  file_line { 'modify BAMBOO_HOME variable':
+    path => $setenv,  
+    line => "BAMBOO_HOME=$bamboo_home",
+    match   => "#BAMBOO_HOME=\"\"",
   }
+
+
 
   service { 'bamboo':
     ensure     => running,
@@ -52,9 +59,7 @@ $tarball="atlassian-bamboo-$version.tar.gz"
     start      => "su ec2-user -c $bamboo_install_dir/bin/start-bamboo.sh",
     stop       => "su ec2-user -c $bamboo_install_dir/bin/stop-bamboo.sh",
 	status     => "puppet:///modules/bamboo/bamboo-status.sh",
-    require    => [ File["/etc/environment"], 
-					Exec[ "change_owners" ] 
-					]
+    require    => Exec[ "change_owners" ]
   }
  
 }
