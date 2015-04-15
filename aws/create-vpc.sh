dd79757709c8a4a1dfbcec3458d1f9de1506d571
Adam <aconfino@gmail.com>
2015-04-15 11:01:13 -0400

    subnet

--- aws/create-vpc.sh
+++ aws/create-vpc.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-vpc_id=vpc-23426746
 home_cidr=98.115.186.136/32
 
-function createVpc(){
-  vpc_id=$(aws ec2 create-vpc --cidr-block 10.0.0.0/16 --output text --query Vpc.VpcId)
-  echo "Created vpc $vpc_id"
-}
+vpc_id=vpc-23426746
+security_group_id=sg-35692c51
 
 function checkState(){
   current_state=$($1)
@@ -19,19 +16,22 @@ function checkState(){
 	  ((counter=counter+5))
       echo "State is $current_stat...$counter secs"
   done
-  echo "State is $current_state"
 }
 
 function getVpcState(){
    aws ec2 describe-vpcs --vpc-id $vpc_id --output text --query Vpcs[0].State
 }
 
+function createVpc(){
+  vpc_id=$(aws ec2 create-vpc --cidr-block 10.0.0.0/16 --output text --query Vpc.VpcId)
+  echo "Created vpc $vpc_id"
+  checkState getVpcState "available"
+  echo "VPC is available"
+}
+
 function enableDnsHostname(){
-    echo $vpc_id
-    checkState getVpcState "available"
 	aws ec2 modify-vpc-attribute --vpc-id $vpc_id --enable-dns-hostnames
 	echo "enabled dns hostnames for $vpc_id"
-
 }
 
 function createSecurityGroup(){
@@ -45,6 +45,19 @@ function createSecurityGroup(){
 	echo "Allowed my ip to communicate over ports 8000-9000 to any instance in the group"
 }
 
+function getSubnetState(){
+    aws ec2 describe-subnets --subnet-id $subnet_id --output text --query Subnets[0].State
+}
+
+function createDefaultSubnet(){
+	subnet_id=$(aws ec2 create-subnet --vpc-id $vpc_id --cidr-block 10.0.1.0/24 --output text --query Subnet.SubnetId)
+	echo "Created default subnet $subnet_id"
+	checkState getSubnetState "available"
+	echo "Subnet is available"
+}
+
+
 #createVpc
 #enableDnsHostname
-createSecurityGroup
+#createSecurityGroup
+#createDefaultSubnet
