aacb8941af218c5465c6d950f70db2a1c4aa1c11
Adam <aconfino@gmail.com>
2015-04-16 14:47:20 -0400

    nexus

--- puppet-scripts/nexus/modules/nexus/manifests/init.pp
+++ puppet-scripts/nexus/modules/nexus/manifests/init.pp
@@ -1,6 +1,5 @@
 class nexus ( $nexus_base_dir, $version) {
 
-$nexus_install_dir="$nexus_base_dir/nexusqube-$version"
 $tarball="nexus-$version-bundle.tar.gz"
 $nexus_install_dir="$nexus_base_dir/nexus-$version-bundle"
 
