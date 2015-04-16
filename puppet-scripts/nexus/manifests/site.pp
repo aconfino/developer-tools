c42f3a5e40b48e021b9495a66bd77b0a5ac5247a
Adam <aconfino@gmail.com>
2015-04-16 14:39:08 -0400

    nexus

---
+++ puppet-scripts/nexus/manifests/site.pp
@@ -0,0 +1,16 @@
+include java
+
+class { 'nexus' :
+  sonar_base_dir => '/opt/nexus',
+  version => '2.11.2-06',
+}
+
+# need to disable so host can access guest machine
+package { "firewalld":
+  ensure => "purged",
+}
+
+Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ], logoutput => true, user => "ec2-user" }
+Package { allow_virtual => false }
+
+Class ['java'] ->  Class ['nexus']
---
+++ puppet-scripts/nexus/modules/nexus/manifests/init.pp
@@ -0,0 +1,38 @@
+class nexus ( $nexus_base_dir, $version) {
+
+$nexus_install_dir="$nexus_base_dir/nexusqube-$version"
+$tarball="nexus-$version-bundle.tar.gz"
+$nexus_install_dir=$nexus_base_dir/nexus-$version-bundle
+
+  file { $nexus_base_dir :
+      ensure => directory,
+	  owner => "ec2-user",
+	  group => "ec2-user",
+  }
+  
+  exec {
+    'download_nexus':
+	command => "curl -O http://download.sonatype.com/nexus/oss/$tarball",
+    cwd => "$nexus_base_dir",
+    require => File[$nexus_base_dir],
+    creates => "$nexus_base_dir/$tarball",
+  }
+  
+  exec {
+    'extract_nexus':
+	command => "tar xf $tarball",
+    cwd => "$nexus_base_dir",
+    require => Exec['download_nexus'],
+    creates => "$nexus_install_dir",
+  }
+  
+  service { 'nexus':
+    ensure     => running,
+    hasstatus  => true,
+    start      => "su ec2-user -c \"$nexus_install_dir/bin/linux-x86-64/nexus.sh start\"",
+    stop       => "su ec2-user -c \"$nexus_install_dir/bin/linux-x86-64/nexus.sh stop\"",
+	status     => "$nexus_install_dir/bin/linux-x86-64/nexus.sh status",
+    require     => Exec [ 'extract_nexus' ]
+  }
+  
+}
